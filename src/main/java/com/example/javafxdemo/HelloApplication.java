2514602a1c6d3bfd164b119220a913ca61349610
krivy <yul.krivaya@bk.ru>
2022-07-06 18:49:00 +0300

    new_1

--- src/main/java/com/example/javafxdemo/HelloApplication.java
+++ src/main/java/com/example/javafxdemo/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 700, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
@@ -19,5 +19,6 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
         launch();
+        System.out.println("hello!");
     }
 }
